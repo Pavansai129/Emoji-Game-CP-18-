8afb85c27d260ab8f2e2c6c38f2842fcacbf4d67
PavanSai <pavansaivvit@gmail.com>
2023-07-10 21:54:55 +0000

    background color updated

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,24 +3,24 @@ import './index.css'
 // Write your code here.
 const NavBar = props => {
   const {score, topScore, isGamePlayed} = props
-  const getNavbar = () =>
-    isGamePlayed ? null : (
-      <div className="score-container">
-        <p className="score">Score: {score}</p>
-        <p className="score">Top Score: {topScore}</p>
-      </div>
-    )
   return (
-    <div className="navbar-container">
-      <div className="logo-container">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
-          alt="emoji logo"
-        />
-        <h1>Emoji Game</h1>
+    <nav className="navbar-container">
+      <div className="navbar-details-container">
+        <div className="logo-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            alt="emoji logo"
+          />
+          <h1>Emoji Game</h1>
+        </div>
+        {!isGamePlayed && (
+          <div className="score-container">
+            <p className="score">Score: {score}</p>
+            <p className="score">Top Score: {topScore}</p>
+          </div>
+        )}
       </div>
-      {getNavbar()}
-    </div>
+    </nav>
   )
 }
 
